9e1de8027d3ed76b85a50435c228b00a90c75e90
saurabh <saurabhkedar@gmail.com>
2022-04-25 18:24:17 +0530

    created structure for Homepage

---
+++ src/Components/Head.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Head({ n }) {
+  return (
+    <div className='head'>
+      <p>hello X</p>
+      <h1>Task-Box</h1>
+      <p>pending tasks are x</p>
+    </div>
+  )
+}
+
+export default Head
---
+++ src/Components/Task.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Task() {
+  return (
+    <div className='task'>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        excepturi eveniet, numquam facilis earum autem quaerat expedita.
+        Voluptate hic voluptatem impedit minus, expedita laudantium enim non
+        quia nesciunt earum at neque eveniet nihil! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Sapiente repudiandae placeat commodi harum
+        voluptatibus soluta enim repellat quibusdam vero? Deleniti unde
+        excepturi porro odit quis, officiis sapiente, animi tempora ipsum
+        reiciendis recusandae in. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Enim repellendus voluptates incidunt dicta voluptas
+        molestias, nobis obcaecati iusto repellat, a excepturi temporibus
+        dolores beatae doloribus, laborum odit ipsam? Nemo sapiente dolorem
+        alias officia velit a ea, quo tempore libero deleniti assumenda, fugiat
+        provident delectus ad sunt quis in. Vero earum ab doloremque, minima
+        voluptates quisquam. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Nesciunt excepturi eveniet, numquam facilis earum autem quaerat
+        expedita. Voluptate hic voluptatem impedit minus, expedita laudantium
+        enim non quia nesciunt earum at neque eveniet nihil! Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Sapiente repudiandae placeat
+        commodi harum voluptatibus soluta enim repellat quibusdam vero? Deleniti
+        unde excepturi porro odit quis, officiis sapiente, animi tempora ipsum
+        reiciendis recusandae in. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Enim repellendus voluptates incidunt dicta voluptas
+        molestias, nobis obcaecati iusto repellat, a excepturi temporibus
+        dolores beatae doloribus, laborum odit ipsam? Nemo sapiente dolorem
+        alias officia velit a ea, quo tempore libero deleniti assumenda, fugiat
+        provident delectus ad sunt quis in. Vero earum ab doloremque, minima
+        voluptates quisquam.
+      </p>
+    </div>
+  )
+}
+
+export default Task
